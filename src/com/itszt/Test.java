abfbbd5ba3cb19ef1cd098ed9ef37a3855621081
SimplicitiRole、 <292813505@qq.com>
2018-06-23 16:10:59 +0800

    Signed-off-by: SimplicitiRole、 <292813505@qq.com> 把Test放入包中。。

--- src/com/itszt/Test.java
+++ src/com/itszt/Test.java
@@ -4,7 +4,7 @@ package com.itszt;
  * Created by sunzi on 2018/6/23.
  */
 public class Test {
-    public void go(){
-        System.out.println("hehe");
+    public void show(){
+        System.out.println("houhou!!!");
     }
 }
